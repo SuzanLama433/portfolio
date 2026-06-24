b710f47d7708a3e9503da2978eacbe7d550a7945
Sujan Lama <sujanlama2323@gmail.com>
2026-06-24 11:46:38 +0545

    project setup

--- portfolio_project/portfolio_project/settings.py
+++ portfolio_project/portfolio_project/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+]
+EXTERNAL_APPS =[
     'myPortfolio',
 ]
+INSTALLED_APPS.extend(EXTERNAL_APPS)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
