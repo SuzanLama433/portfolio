e6f9e16666c2060024c3be2115dface341878b76
Sujan Lama <sujanlama2323@gmail.com>
2026-06-24 17:29:26 +0545

    update portfolio

--- portfolio_project/myPortfolio/urls.py
+++ portfolio_project/myPortfolio/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',home,name='home')
+    path('',home,name='home'),
+    path('about/',about,name='about'),
+    path('skill/',skills,name='skills'),
+    path('project/',project,name='project'),
+    path('contact/',contact,name='contact')
 ]
--- portfolio_project/myPortfolio/views.py
+++ portfolio_project/myPortfolio/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def home (request):
     return render(request,'myPortfolio/home.html')
+
+def about(request):
+    return render(request,'myPortfolio/about.html')
+
+def skills(request):
+    return render(request,'myPortfolio/skills.html')
+
+def project(request):
+    return render(request,'myPortfolio/projects.html')
+
+def contact(request):
+    return render(request,'myPortfolio/contact.html')
